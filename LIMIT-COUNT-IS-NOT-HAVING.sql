c13200935c2d4795ff50229434bb4d396c0e215c
Katerina <63414886+Gaydaeva@users.noreply.github.com>
2022-12-06 13:19:52 +0300

    Add files via upload

---
+++ LIMIT-COUNT-IS-NOT-HAVING.sql
@@ -0,0 +1,32 @@
+SELECT product_name, unit_price
+FROM products
+	WHERE discontinued <> 1
+		ORDER BY unit_price DESC
+			LIMIT 5;
+			
+SELECT ship_city, ship_region, ship_country
+FROM orders
+	WHERE ship_region IS NULL;
+	
+SELECT ship_city, ship_region, ship_country
+FROM orders
+	WHERE ship_region IS NOT NULL;
+	
+SELECT ship_country, COUNT(*)
+FROM orders
+	WHERE freight > 50
+		GROUP BY ship_country
+			ORDER BY COUNT (*) DESC;
+
+SELECT category_id, SUM(units_in_stock)
+FROM products
+	GROUP BY category_id
+		ORDER BY SUM(units_in_stock) DESC
+			LIMIT 5;
+
+SELECT category_id, SUM(unit_price * units_in_stock)
+FROM products
+	WHERE discontinued <> 1
+		GROUP BY category_id
+			HAVING SUM(unit_price * units_in_stock) > 5000
+				ORDER BY SUM(unit_price * units_in_stock) DESC
