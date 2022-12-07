828f5d3222ec349cd94a8afa672e6685126e19c0
Katerina <63414886+Gaydaeva@users.noreply.github.com>
2022-12-07 16:43:59 +0300

    Update ANY-ALL.sql

--- Подзапросы/ANY-ALL.sql
+++ Подзапросы/ANY-ALL.sql
@@ -8,12 +8,10 @@ FROM customers
 		
 SELECT DISTINCT company_name
 FROM customers
-	WHERE customer_id = ANY(
-						SELECT customer_id
-						FROM orders
-							JOIN order_details USING(order_id)
-								WHERE quantity > 40
-						   )
+	WHERE customer_id = ANY(SELECT customer_id
+				FROM orders
+					JOIN order_details USING(order_id)
+						WHERE quantity > 40)
 							
 -- Выбрать такие продукты, количество которых больше среднего по всем заказам
 
@@ -21,7 +19,7 @@ SELECT DISTINCT product_name, quantity
 FROM products
 	JOIN order_details USING(product_id)
 		WHERE quantity > (SELECT AVG(quantity)
-							FROM order_details)
+				  FROM order_details)
 			ORDER BY quantity
 			
 -- Выбрать все продукты, количество которых больше среднего значения количества 
@@ -31,7 +29,6 @@ SELECT DISTINCT product_name, quantity
 FROM products
 	JOIN order_details USING(product_id)
 		WHERE quantity > ALL(SELECT AVG(quantity)
-								FROM order_details
-									GROUP BY product_id)
-
+					FROM order_details
+						GROUP BY product_id)
 			ORDER BY quantity
