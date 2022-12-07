12c779e5f75941978fc1fdc7090906fdcb865357
Katerina <63414886+Gaydaeva@users.noreply.github.com>
2022-12-07 18:37:57 +0300

    Update Подзапросы.sql

--- Подзапросы/Подзапросы.sql
+++ Подзапросы/Подзапросы.sql
@@ -4,8 +4,8 @@
 SELECT product_name, units_in_stock
 FROM products
 	WHERE units_in_stock < ALL(SELECT AVG(quantity)
-								FROM order_details
-									GROUP BY product_id)
+			           FROM order_details
+			           	GROUP BY product_id)
 		ORDER BY units_in_stock DESC
 
 -- Напишите запрос, который выводит общую сумму фрахтов заказов для компаний-заказчиков для заказов, стоимость фрахта 
@@ -16,8 +16,8 @@ FROM products
 SELECT customer_id, SUM(freight) AS freight_sum
 FROM orders
 	INNER JOIN (SELECT customer_id, AVG(freight) AS freight_avg
-				      FROM orders
-					       GROUP BY customer_id) AS a USING(customer_id) 
+		    FROM orders
+		   	GROUP BY customer_id) AS a USING(customer_id) 
 		WHERE freight > freight_avg AND shipped_date BETWEEN '1996-07-16' AND '1996-07-31'
 			GROUP BY customer_id
 				ORDER BY freight_sum
@@ -30,10 +30,10 @@ FROM orders
 SELECT customer_id, ship_country, order_price
 FROM orders
   JOIN (SELECT order_id, SUM(unit_price * quantity - unit_price * quantity * discount) AS order_price
-	      FROM order_details
-	        GROUP BY order_id) AS od USING(order_id)
+	FROM order_details
+		GROUP BY order_id) AS od USING(order_id)
 			WHERE ship_country IN('Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia',
-								            'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Uruguay', 'Venezuela')
+					      'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Uruguay', 'Venezuela')
 				AND order_date >= '1997-09-01'
 					ORDER BY order_price DESC
 						LIMIT 3
@@ -48,5 +48,5 @@ FROM products
 SELECT product_name
 FROM products
 WHERE product_id = ANY(SELECT product_id
-					             FROM order_details
-					   		         WHERE quantity = 10)
+			 FROM order_details
+			 	WHERE quantity = 10)
